59d17d575013b2e05b9b9296f5d7e41860e74bb2
yulian-laptop <jweiss0704@gmail.com>
2025-02-11 18:11:41 +0100

    Initial Commit 2

---
+++ Editor/IsEditingGlobalShaderVariableController.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class IsEditingGlobalShaderVariableController
+{
+    static IsEditingGlobalShaderVariableController()
+    {
+        EditorApplication.playModeStateChanged += PlayModeStateUpdated;
+    }
+
+    private static void PlayModeStateUpdated(PlayModeStateChange change)
+    {
+        switch (change)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+                Shader.SetGlobalInteger("_ISEDITING", 1);
+                break;
+            case PlayModeStateChange.ExitingEditMode:
+                Shader.SetGlobalInteger("_ISEDITING", 0);
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                Shader.SetGlobalInteger("_ISEDITING", 0);
+                break;
+            case PlayModeStateChange.ExitingPlayMode:
+                Shader.SetGlobalInteger("_ISEDITING", 1);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Utility/Editor/get-is-editing.hlsl
@@ -0,0 +1,16 @@
+//Make sure its only included once!
+#ifndef ISEDITING_INCLUDED
+#define ISEDITING_INCLUDED
+
+int _ISEDITING;
+
+void getIsEditing_float(out int isEditing)
+{
+    isEditing = _ISEDITING;
+}
+
+void getIsEditing_half(out int isEditing)
+{
+    isEditing = _ISEDITING;
+}
+#endif
