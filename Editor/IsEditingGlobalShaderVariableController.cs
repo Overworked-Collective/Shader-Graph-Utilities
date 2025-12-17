33ed15dda36be6df42e2d25cc3dd660f7654472e
yulian-weiss <jweiss0704@gmail.com>
2025-12-17 15:54:18 +0100

    Testing to make sure initialized as 1

--- Editor/IsEditingGlobalShaderVariableController.cs
+++ Editor/IsEditingGlobalShaderVariableController.cs
@@ -7,6 +7,7 @@ public static class IsEditingGlobalShaderVariableController
     static IsEditingGlobalShaderVariableController()
     {
         EditorApplication.playModeStateChanged += PlayModeStateUpdated;
+				Shader.SetGlobalInteger("_ISEDITING", 1);
     }
 
     private static void PlayModeStateUpdated(PlayModeStateChange change)
@@ -14,16 +15,20 @@ public static class IsEditingGlobalShaderVariableController
         switch (change)
         {
             case PlayModeStateChange.EnteredEditMode:
-                Shader.SetGlobalInteger("_ISEDITING", 1);
+                //Shader.SetGlobalInteger("_ISEDITING", 1);
+								Shader.SetGlobalInteger("_ISEDITING", 2);
                 break;
             case PlayModeStateChange.ExitingEditMode:
-                Shader.SetGlobalInteger("_ISEDITING", 0);
+                //Shader.SetGlobalInteger("_ISEDITING", 0);
+								Shader.SetGlobalInteger("_ISEDITING", 3);
                 break;
             case PlayModeStateChange.EnteredPlayMode:
-                Shader.SetGlobalInteger("_ISEDITING", 0);
+                //Shader.SetGlobalInteger("_ISEDITING", 0);
+								Shader.SetGlobalInteger("_ISEDITING", 4);
                 break;
             case PlayModeStateChange.ExitingPlayMode:
-                Shader.SetGlobalInteger("_ISEDITING", 1);
+                //Shader.SetGlobalInteger("_ISEDITING", 1);
+								Shader.SetGlobalInteger("_ISEDITING", 5);
                 break;
             default:
                 break;
